b1775925a8ad8681b4f901d57809b374485c2db4
Selamawit-Kassaye <selamawitkassaye2019@gmail.com>
2022-12-01 19:41:34 +0300

    importing python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+from sys import argv
 if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+    n = 0
+    for i in range(len(argv) - 1):
+        n += int(argv[i + 1])
+    print("{:d}".format(n))
